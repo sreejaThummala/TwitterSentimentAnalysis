9f51c85c0845dee9fa0bdca89f6801de4311e10a
sreejaThummala <sthummala@wisc.edu>
2014-01-07 22:16:58 -0500

    Create Attributes.java

---
+++ SWN/Attributes.java
@@ -0,0 +1,17 @@
+public class Attributes{
+        	private int hits;
+        	private int length;
+        	
+        	Attributes(int hits,int length){
+        	this.hits = hits;
+        	this.length = length;
+        	}
+        	
+        	public int getHits(){
+        	return this.hits;
+        	}
+        	
+        	public int getLength(){
+        	return this.length;
+          }
+        }
